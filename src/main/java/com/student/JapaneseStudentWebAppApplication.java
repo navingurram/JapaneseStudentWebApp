d3fa076ba01525bbd216e64590358b85bd624c8e
Sahil <sahilrangari07@gmail.com>
2024-05-16 11:58:54 +0530

    First Commit

---
+++ src/main/java/com/student/JapaneseStudentWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseStudentWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseStudentWebAppApplication.class, args);
+	}
+
+}
